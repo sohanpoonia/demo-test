f4bd34843c2b94403561d1662dc3dab4cb9f02b9
Sohan Lal <sohan.lal@startupfarms.corp>
2017-12-15 17:02:32 +0530

    make it better

---
+++ src/main/java/com/example/service/impl/CounterServiceInterfaceImpl.java
@@ -0,0 +1,43 @@
+package com.example.service.impl;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.stereotype.Service;
+
+import com.example.exception.ApplicationException;
+import com.example.model.CounterModel;
+import com.example.service.CounterServiceInterface;
+import com.example.utility.DateUtility;
+
+@Service
+public class CounterServiceInterfaceImpl implements CounterServiceInterface {
+	private AtomicInteger counter=new AtomicInteger();
+	@Override
+	public CounterModel fetchCurrentTimeAndCount() throws ApplicationException {
+		CounterModel counterModel=new CounterModel();
+		counterModel.setCalls(counter.incrementAndGet());
+		counterModel.setTimestamp(DateUtility.formatCurrentDate());
+		return null;
+	}
+
+	@Override
+	public CounterModel create(CounterModel object) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CounterModel update(CounterModel object) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CounterModel delete(CounterModel object) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
